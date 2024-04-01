e0b585e3f5d22c0c49553eb594aefdd608b9a5cf
SHUBHAM CHODVADIYA <shubhamchodavadiya1122@gmail.com>
2024-04-01 23:26:15 +0530

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
     game: Game;
@@ -8,10 +9,13 @@ const GameCard = ({ game }: Props) => {
     return (
         <Card borderRadius={10} overflow={"hidden"}>
             <Image src={game.background_image} />
-            <CardBody>
-                <Heading fontSize={"2xl"}>{game.name}</Heading>
+            <CardBody paddingBottom={0}>
+                <Heading size={'md'} padding={0}>{game.name}</Heading>
             </CardBody>
-        </Card>
+            <PlatformIconList
+                platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+        </Card >
     );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,13 +2,20 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platform }[];
 }
 
-interface FatchGamesResponse {
+interface FetchGamesResponse {
     count: number;
     results: Game[];
 }
@@ -19,10 +26,9 @@ const useGames = () => {
     useEffect(() => {
         const controller = new AbortController();
         apiClient
-            .get<FatchGamesResponse>("/games", { signal: controller.signal })
+            .get<FetchGamesResponse>("/games", { signal: controller.signal })
             .then((res) => {
                 setGames(res.data.results);
-                console.log(res.data.results);
                 setError("");
             })
             .catch((err) => {
